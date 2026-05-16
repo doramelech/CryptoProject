f7a41da9c06f05268f194aec3c5ec0a38e2de968
10User <dor.shtrom@Gmail.com>
2026-05-16 16:37:19 +0300

    changed default GetTime to None

--- INTERFACE.py
+++ INTERFACE.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from time_based_cipher import TBC
 from yonch_cipher import YonchCipher
-def general_cipher(text, cipher, mode, time_value = GetTime() ):
+
+def general_cipher(text, cipher, mode, time_value=None ):
     try:
         if cipher == "TBC":
             return TBC(text, time_value, mode)
