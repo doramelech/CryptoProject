ee021e5142a19acba0cf7bedbbd87b9f755b477b
10User <dor.shtrom@Gmail.com>
2026-05-16 16:31:39 +0300

    added general cipher function

--- INTERFACE.py
+++ INTERFACE.py
@@ -1,5 +1,13 @@
 import streamlit as st
 from time_based_cipher import TBC
 from yonch_cipher import YonchCipher
+def general_cipher(text, cipher, mode, time_value = GetTime() ):
+    try:
+        if cipher == "TBC":
+            return TBC(text, time_value, mode)
+        return YonchCipher(text, mode)
+    except Exception as e:
+        return e
+
 
 
