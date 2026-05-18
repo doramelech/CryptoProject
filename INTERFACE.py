ef2cbe43c8c092a2607f05e607c3200eab0420c2
10User <dor.shtrom@Gmail.com>
2026-05-18 15:17:22 +0300

    removed swap function

--- INTERFACE.py
+++ INTERFACE.py
@@ -122,8 +122,6 @@ def auth_page():
                 else:
                     st.error(message)
 
-def swap(str1, str2):
-    return str2, str1
 
 def general_cipher(text, cipher, mode, time_value=None):
     try:
